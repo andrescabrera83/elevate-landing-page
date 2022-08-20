33876c1a7c4de78d62c7c59d41d33a6d831ac550
Andres Cabrera <andresexplorer83@gmail.com>
2022-08-20 11:58:16 -0300

    Simple React Frontend Base

---
+++ src/components/SelectService.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+let serviceName;
+
+class SelectService extends React.Component {
+
+    render(){
+        return(
+            <div className="dropdown">
+                <select>
+                    <option value="">daora</option>
+                </select>
+            </div>
+        )
+    }
+}
+
+export default SelectService;
